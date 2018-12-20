75088783b7d8b400fa995afb72569a6798d5df69
Shawn Nostrom <s_nostrom@hotmail.com>
2018-12-19 19:21:01 -0700

    server files

--- server/index.js
+++ server/index.js
@@ -5,14 +5,17 @@ const cors = require('cors');
 const logger = require('morgan')
 const massive = require ('massive');
 const bodyParser = require('body-parser');
-
+const path = require('path')
 
 
 const app = express();
 
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use(logger('tiny'))
+app.use( express.static( `${__dirname}/../build` ) );
+
 
 massive (process.env.dataBase)
 .then ( db => {
@@ -25,5 +28,7 @@ massive (process.env.dataBase)
 
 
 
-
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 app.listen(8080 , () => console.log('listening on 8080'))
